a36aa1560ca2bfbaa9a9c5f804440e04c1f03a7b
Yoginth <me@yoginth.ml>
2018-02-17 17:53:43 +0000

    Updated Blade - Register

--- layout/docspen/auth/register.blade.php
+++ layout/docspen/auth/register.blade.php
@@ -1,7 +1,7 @@
 @extends('public')
 
 @section('header-buttons')
-    <a href="{{ baseUrl("/login") }}"><i class="zmdi zmdi-sign-in"></i>{{ trans('auth.log_in') }}</a>
+    <a href="{{ baseUrl("/login") }}">@icon('login') {{ trans('auth.log_in') }}</a>
 @stop
 
 @section('content')
