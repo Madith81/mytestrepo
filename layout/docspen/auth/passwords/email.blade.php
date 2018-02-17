d79eac06a70c2f3a5b46d44864a1234154af6b88
Yoginth <me@yoginth.ml>
2018-02-17 17:52:07 +0000

    Updated Blade - Reset

--- layout/docspen/auth/passwords/email.blade.php
+++ layout/docspen/auth/passwords/email.blade.php
@@ -1,7 +1,7 @@
 @extends('public')
 
 @section('header-buttons')
-    <a href="{{ baseUrl("/login") }}"><i class="@icon('login') {{ trans('auth.log_in') }}</a>
+    <a href="{{ baseUrl("/login") }}">@icon('login') {{ trans('auth.log_in') }}</a>
     @if(setting('registration-enabled'))
         <a href="{{ baseUrl("/register") }}">@icon('new-user') {{ trans('auth.sign_up') }}</a>
     @endif
