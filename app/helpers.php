67ef421c4597cf962733735e5359adaed0b0086b
Yoginth <me@yoginth.ml>
2018-02-17 22:49:03 +0530

    Made it possible to override icons via custom theme

--- app/helpers.php
+++ app/helpers.php
@@ -1,13 +1,5 @@
 <?php
 
-/**
- * Copyright (c) 2017-present, DocsPen.
- * All rights reserved.
- *
- * This source code is licensed under the license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 use DocsPen\Ownable;
 
 /**
@@ -48,7 +40,6 @@ function cdn($file = '')
 /**
  * Helper method to get the current User.
  * Defaults to public 'Guest' user if not logged in.
- *
  * @return \DocsPen\User
  */
 function user()
@@ -58,22 +49,19 @@ function user()
 
 /**
  * Check if current user is a signed in user.
- *
  * @return bool
  */
 function signedInUser()
 {
-    return auth()->user() && ! auth()->user()->isDefault();
+    return auth()->user() && !auth()->user()->isDefault();
 }
 
 /**
  * Check if the current user has a permission.
  * If an ownable element is passed in the jointPermissions are checked against
  * that particular item.
- *
  * @param $permission
  * @param Ownable $ownable
- *
  * @return mixed
  */
 function userCan($permission, Ownable $ownable = null)
@@ -84,16 +72,13 @@ function userCan($permission, Ownable $ownable = null)
 
     // Check permission on ownable item
     $permissionService = app(\DocsPen\Services\PermissionService::class);
-
     return $permissionService->checkOwnableUserAccess($ownable, $permission);
 }
 
 /**
  * Helper to access system settings.
- *
  * @param $key
  * @param bool $default
- *
  * @return bool|string|\DocsPen\Services\SettingService
  */
 function setting($key = null, $default = false)
@@ -102,22 +87,19 @@ function setting($key = null, $default = false)
     if (is_null($key)) {
         return $settingService;
     }
-
     return $settingService->get($key, $default);
 }
 
 /**
  * Helper to create url's relative to the applications root path.
- *
  * @param string $path
- * @param bool   $forceAppDomain
- *
+ * @param bool $forceAppDomain
  * @return string
  */
 function baseUrl($path, $forceAppDomain = false)
 {
     $isFullUrl = strpos($path, 'http') === 0;
-    if ($isFullUrl && ! $forceAppDomain) {
+    if ($isFullUrl && !$forceAppDomain) {
         return $path;
     }
     $path = trim($path, '/');
@@ -133,30 +115,78 @@ function baseUrl($path, $forceAppDomain = false)
         return url($path);
     }
 
-    return rtrim(config('app.url'), '/').'/'.$path;
+    return rtrim(config('app.url'), '/') . '/' . $path;
 }
 
+/**
+ * Get an instance of the redirector.
+ * Overrides the default laravel redirect helper.
+ * Ensures it redirects even when the app is in a subdirectory.
+ *
+ * @param  string|null  $to
+ * @param  int     $status
+ * @param  array   $headers
+ * @param  bool    $secure
+ * @return \Illuminate\Routing\Redirector|\Illuminate\Http\RedirectResponse
+ */
+function redirect($to = null, $status = 302, $headers = [], $secure = null)
+{
+    if (is_null($to)) {
+        return app('redirect');
+    }
+
+    $to = baseUrl($to);
+
+    return app('redirect')->to($to, $status, $headers, $secure);
+}
+
+/**
+ * Get a path to a theme resource.
+ * @param string $path
+ * @return string|boolean
+ */
+function theme_path($path = '')
+{
+    $theme = config('view.theme');
+    if (!$theme) {
+        return false;
+    }
+
+    return base_path('themes/' . $theme .($path ? DIRECTORY_SEPARATOR.$path : $path));
+}
+
+/**
+ * Get fetch an SVG icon as a string.
+ * Checks for icons defined within a custom theme before defaulting back
+ * to the 'resources/assets/icons' folder.
+ * @param $name
+ * @param array $attrs
+ * @return mixed
+ */
 function icon($name, $attrs = [])
 {
-    $iconPath = resource_path('assets/icons/'.$name.'.svg');
     $attrString = ' ';
     foreach ($attrs as $attrName => $attr) {
-        $attrString .= $attrName.'="'.$attr.'" ';
+        $attrString .=  $attrName . '="' . $attr . '" ';
     }
-    $fileContents = file_get_contents($iconPath);
 
-    return  str_replace('<svg', '<svg'.$attrString, $fileContents);
+    $iconPath = resource_path('assets/icons/' . $name . '.svg');
+    $themeIconPath = theme_path('icons/' . $name . '.svg');
+    if ($themeIconPath && file_exists($themeIconPath)) {
+        $iconPath = $themeIconPath;
+    }
+
+    $fileContents = file_get_contents($iconPath);
+    return  str_replace('<svg', '<svg' . $attrString, $fileContents);
 }
 
 /**
  * Generate a url with multiple parameters for sorting purposes.
  * Works out the logic to set the correct sorting direction
  * Discards empty parameters and allows overriding.
- *
  * @param $path
  * @param array $data
  * @param array $overrideData
- *
  * @return string
  */
 function sortUrl($path, $data, $overrideData = [])
@@ -176,12 +206,12 @@ function sortUrl($path, $data, $overrideData = [])
         if ($trimmedVal === '') {
             continue;
         }
-        $queryStringSections[] = urlencode($name).'='.urlencode($trimmedVal);
+        $queryStringSections[] = urlencode($name) . '=' . urlencode($trimmedVal);
     }
 
     if (count($queryStringSections) === 0) {
         return $path;
     }
 
-    return baseUrl($path.'?'.implode('&', $queryStringSections));
+    return baseUrl($path . '?' . implode('&', $queryStringSections));
 }
