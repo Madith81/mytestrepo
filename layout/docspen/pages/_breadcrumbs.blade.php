22b24f44a55b0d3e2306b664c44f15a5248d72ab
Yoginth <me@yoginth.ml>
2018-02-18 09:47:01 +0530

    Updated to SVG Icons on Pages

--- layout/docspen/pages/_breadcrumbs.blade.php
+++ layout/docspen/pages/_breadcrumbs.blade.php
@@ -12,5 +12,5 @@
         <img src="https://cdnjs.cloudflare.com/ajax/libs/uswds/1.4.4/img/arrow-right.svg" style="height:10px">
     @endif
 
-    <a href="{{ $page->getUrl() }}" class="text-page text-button"><i class="zmdi zmdi-file"></i>{{ $page->getShortName() }}</a>
+    <a href="{{ $page->getUrl() }}" class="text-page text-button">@icon('page'){{ $page->getShortName() }}</a>
 </div>
